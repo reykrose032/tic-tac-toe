4b7e26cebd71548cb535e78eb8c7c304b2534aa3
Kevin <rkvnh032gmail.com>
2022-05-09 12:51:36 -0700

    init

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            Copyright Ninja 2022
+        </div>
+    );
+}
+ 
+export default Footer
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Footer from "../comps/Footer"
+import Navbar from "../comps/Navbar"
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to Next.js!</h1>
+            <p>Lorem Ipsum</p>
+        </div>
+    )
+}
+
+export default HomePage
